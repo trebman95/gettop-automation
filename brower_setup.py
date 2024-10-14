e67481c4b02540f581bc8bc4337aeead4622540e
Tre Blackman <trebman91@gmail.com>
2024-10-14 00:46:00 -0400

    Lesson 3 Done

--- brower_setup.py
+++ brower_setup.py
@@ -15,3 +15,8 @@ browser.find_element(By.ID, 'password') #Password #login pop-up
 browser.find_element(By.XPATH, "//label[@for='user_login']") #Username text above input field, forgot password
 browser.find_element(By.XPATH, "//button[@value='Reset password]") #Reset password button
 browser.find_element(By.XPATH, "//i[@class='icon-user']") #User icon (top right, next to cart)
+browser.find_element(By.ID,   'user_login') #Username input field
+browser.find_element(By.XPATH, "//h1[text()='My Account']") #MY ACCOUNT text
+browser.find_element(By.XPATH, "//a[text()='Accessories' and @class='nav-top-link']") #Accessories link in header
+browser.find_element(By.XPATH, "//p[contains(text(),'Lost your password?')]" ) #“Lost your password…” text
+browser.find_element(By.XPATH, "//ul[@id = 'menu-laptop-1']//a[text()='Accessories']") #Accessories link in footer
