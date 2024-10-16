32f4819a111b251ff0f85f3c89f543a7e47a6816
Tre Blackman <trebman91@gmail.com>
2024-10-16 00:30:46 -0400

    almost done

--- tests/test_gettop.py
+++ tests/test_gettop.py
@@ -3,38 +3,45 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from time import sleep
+import pytest
 
 
 class TestGetTop:
+    browser = None
+
+    def setup(self):
+        # Install and start the browser
+        driver_path = ChromeDriverManager().install()
+        self.browser = webdriver.Chrome(service=Service(driver_path))
 
     def test_header_link_tablet(self):
-        # Install and start the browser
-        driver_path = ChromeDriverManager().install()
-        browser = webdriver.Chrome(service=Service(driver_path))
-
         # Click 'Tablets' link on the homepage
-        browser.get('https://gettop.us/')
-        browser.find_element(By.XPATH, "//a[text()='Tablets' and @class='nav-top-link']").click()#Tablet top header
-
+        self.browser.get('https://gettop.us/')
+        self.browser.find_element(By.XPATH, "//a[text()='Tablets' and @class='nav-top-link']").click()
         #Verify correct page opens
         expected_result = 'HOME / TABLET'
-        actual_result = browser.find_element(By.XPATH, "//nav[contains(@class,'breadcrumbs')]").text
+        actual_result = self.browser.find_element(By.XPATH, "//nav[contains(@class,'breadcrumbs')]").text
         assert actual_result == expected_result
 
-        browser.quit()
 
     def test_empty_cart(self):
-        # Install and start the browser
-        driver_path = ChromeDriverManager().install()
-        browser = webdriver.Chrome(service=Service(driver_path))
-
         # Click Cart icon on the homepage
-        browser.get('https://gettop.us/')
-        browser.find_element(By.XPATH, "//a[contains(@class,'header-cart-link is-small')]").click()#Cart top header
-
+        self.browser.get('https://gettop.us/')
+        self.browser.find_element(By.XPATH, "//a[contains(@class,'header-cart-link is-small')]").click()
         #Verify that the empy cart message is displayed on the next page
         expected_result = 'Your cart is currently empty.'
-        actual_result = browser.find_element(By.XPATH, "//p[text()='Your cart is currently empty.']").text
+        actual_result = self.browser.find_element(By.XPATH, "//p[text()='Your cart is currently empty.']").text
         assert actual_result == expected_result
 
-        browser.quit()
+    def test_login_form(self):
+        # Click on user profile icon on the homepage (top right, next to cart)
+        self.browser.get('https://gettop.us/')
+        self.browser.find_element(By.XPATH, "//a[@class='nav-top-link nav-top-not-logged-in']").click()
+        #Verify that the empy cart message is displayed on the next page
+        expected_result = 'LOGIN'
+        actual_result = self.browser.find_element(By.XPATH, "//h3[text()='Login']").text
+        assert actual_result == expected_result
+
+
+    def teardown(self):
+        self.browser.quit()
